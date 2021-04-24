c2002efcc9c7f1faec5d2710c32b04874ef43e37
lak997 <68535023+lak997@users.noreply.github.com>
2021-04-25 01:24:21 +0530

    Technical Assistant completed

---
+++ Assist/src/main/java/com/GadgetBadget/util/User.java
@@ -0,0 +1,36 @@
+package com.GadgetBadget.util;
+
+public class User {
+	private String 	tipId;
+	private String relatedArea;
+	private String tipDetail;
+	private String date;
+	
+	
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTipId() {
+		return tipId;
+	}
+	public void setTipId(String tipId) {
+		this.tipId = tipId;
+	}
+	public String getRelatedArea() {
+		return relatedArea;
+	}
+	public void setRelatedArea(String relatedArea) {
+		this.relatedArea = relatedArea;
+	}
+	public String getTipDetail() {
+		return tipDetail;
+	}
+	public void setTipDetail(String tipDetail) {
+		this.tipDetail = tipDetail;
+	}
+	
+	
+}
